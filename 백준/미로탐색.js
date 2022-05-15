393da651f8520958caba910c7a8286e197065353
ahn0min <ahn0min95@gmail.com>
2022-05-16 02:21:38 +0900

    fix: 조금 더 자연스러운 코드로 변경

--- 백준/미로탐색.js
+++ 백준/미로탐색.js
@@ -1,39 +1,41 @@
-const sol = (input) => {
-  const [N, M] = input[0].split(" ").map((v) => +v);
-  const adjM = [];
-  for (let i = 1; i <= N; i++) adjM.push(input[i].split("").map((v) => +v)); // 미로 행렬
-  const check = Array.from({ length: N }, () => Array(M).fill(0)); // 방문 여부를 위한 체크 행렬
+const fs = require('fs');
+const input = fs.readFileSync('/stdin/dev').toString().trim().split('\n');
 
-  function bfs(row, col) {
-    const dx = [-1, 0, 1, 0];
-    const dy = [0, 1, 0, -1]; // 현재 위치로부터 동서남북 조회를 위한 dx, dy 배열
-    const queue = [];
-    queue.push([row, col]);
-    check[row][col] = 1;
-    while (queue.length) {
-      const [x, y] = queue.shift(); // 큐는 FIFO이므로, 맨 앞부터 꺼낸다.
-      for (let i = 0; i < 4; i++) {
-        const [nx, ny] = [x + dx[i], y + dy[i]]; // (nx, ny)는 이동 가능성이 있는 좌표.
-        if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue; // 미로를 벗어나는 좌표는 제외
-        if (adjM[nx][ny] && !check[nx][ny]) { // 길이 있고, 방문하지 않았다면 방문
-          check[nx][ny] = check[x][y] + 1; // (x,y)의 값이 (x,y)까지 최단경로에 해당한다.
-          queue.push([nx, ny]); // BFS(너비 우선)로 현재 위치에서 갈 수 있는 좌표를 모두 큐에 넣어준다.
+function solution(input) {
+    // 여기서 X, Y 는 입력시의 그래프 축을 90도 돌리는 느낌으로 진행한다.
+    const [X, Y] = input[0].split(" ").map((v) => +v);
+    const graph = [];
+    for (let i = 1; i <= X; i++) graph.push(input[i].split("").map((v) => +v)); // 미로 행렬
+    const visited = Array.from({ length: X }, () => Array(Y).fill(0)); // 방문 여부를 위한 체크 행렬
+    
+
+    // 너비 우선 탐색하여 원점에서부터 가까운 지점부터 탐색
+    function BFS(firstX, firstY) {
+        // 이동시에 기존 좌표에서 더해줄 좌표들
+        const dx = [0, 1, 0, -1];
+        const dy = [1, 0, -1, 0];
+        const queue = [];
+        queue.push([firstX, firstY]);
+        // 원정에서 시작한다는 것은 방문을 한 것이기 때문에 1 로 해준다.
+        visited[firstX][firstY] = 1;
+        while (queue.length) {
+            const [x, y] = queue.shift(); // queue 자료구조이기 때문에 FIFO(First In First Out)
+            // 4방향을 모두 체크
+            for (let i = 0; i < 4; i++) {
+                const [nx, ny] = [x + dx[i], y + dy[i]];
+                // 정해진 좌표를 벗어날 경우 뒤의 코드를 실행하지 않고 다음으로 넘어감
+                if(nx < 0 || ny < 0 || nx >= X || ny >= Y) continue;
+                // 해당 좌표가 갈 수 있는 길(1) 이면서 방문하지 않은 곳(0) 일 경우에 다음 로직을 실행함
+                if(graph[nx][ny] && !visited[nx][ny]) {
+                    // 탐색중이 queue에 집어넣고 해당 좌표의 원점으로부터의 거리는 이전 좌표 + 1 이다.
+                    visited[nx][ny] = visited[x][y] + 1
+                    queue.push([nx, ny]);
+                }
+            }
         }
-      }
     }
-  }
-  bfs(0, 0);
-  return check[N - 1][M - 1];
-};
+    BFS(0, 0);
+    return visited[X - 1][Y - 1]
+}
 
-// 백준에서 입력을 받기 위한 코드
-const input = [];
-require("readline")
-  .createInterface(process.stdin, process.stdout)
-  .on("line", (line) => {
-    input.push(line);
-  })
-  .on("close", () => {
-    console.log(sol(input));
-    process.exit();
-  });
+console.log(solution(input))
